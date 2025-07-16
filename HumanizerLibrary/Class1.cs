c65758e5cae01a85cf20734c0ef9bad9968bb297
john6604 <jchimbo456@gmail.com>
2025-07-15 23:30:11 -0500

    Humanizer Project con .NET 4.8

---
+++ HumanizerLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Humanizer;
+
+namespace MyExtensions
+{
+    public class TextHelper
+    {
+        public string NumeroATexto(int numero)
+        {
+            return numero.ToWords(new System.Globalization.CultureInfo("es-ES"));
+        }
+    }
+}
