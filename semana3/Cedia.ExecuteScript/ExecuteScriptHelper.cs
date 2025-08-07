91c95c590e4c5cacdd4f0328f2ec3b9db69d4f12
john6604 <jchimbo456@gmail.com>
2025-08-07 10:18:15 -0500

    Cedia.ExecuteScript

---
+++ semana3/Cedia.ExecuteScript/ExecuteScriptHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace Cedia.ExecuteScript
+{
+    public class ExecuteScriptHelper
+    {
+        public static string RunPythonProcedure(string scriptPath, string libPath, string procedure)
+        {
+            try
+            {
+                ScriptEngine pythonEngine = Python.CreateEngine();
+
+                ICollection<string> paths = pythonEngine.GetSearchPaths();
+                paths.Add(libPath);
+                pythonEngine.SetSearchPaths(paths);
+
+                ScriptSource pythonScript = pythonEngine.CreateScriptSourceFromFile(scriptPath);
+                CompiledCode code = pythonScript.Compile();
+
+                ScriptScope scope = pythonEngine.CreateScope();
+
+                code.Execute(scope);
+
+                var result = pythonEngine.Execute(procedure, scope);
+
+                return result?.ToString() ?? "";
+            }
+            catch (Exception ex)
+            {
+                return $"ERROR: {ex.Message}\nTYPE: {ex.GetType().Name}\nMETHOD: {ex.TargetSite}\nSTACKTRACE: {ex.StackTrace}";
+            }
+        }
+    }
+}
